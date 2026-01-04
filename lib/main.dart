95e71eec46dc7eaa8684ec126b08bb6ac76db1b4
wasimGhoniem <wasimghoniem@gmail.com>
2026-01-05 01:56:24 +0200

    Based on the provided diff, here is the commit message:
    fix: enable DevicePreview only in debug mode
    
    - Updated `DevicePreview` configuration in `main.dart` to set `enabled` to `!kReleaseMode`, ensuring it is active during development but disabled in release builds.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -28,14 +26,11 @@ void main() async {
   await configureDependencies();
 
   runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => EasyLocalization(
-        supportedLocales: AppConstants.supportedLocales,
-        path: AppConstants.assetsPath,
-        fallbackLocale: const Locale(AppConstants.en),
-        child: PortFolio(),
-      ),
+    EasyLocalization(
+      supportedLocales: AppConstants.supportedLocales,
+      path: AppConstants.assetsPath,
+      fallbackLocale: const Locale(AppConstants.en),
+      child: PortFolio(),
     ),
   );
 }
