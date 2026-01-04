6de106b45943902d57191d1be3beb70a2d4ea56b
wasimGhoniem <wasimghoniem@gmail.com>
2026-01-04 21:37:41 +0200

    edit image

--- lib/core/widgets/personal_image.dart
+++ lib/core/widgets/personal_image.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:portfolio_website/core/config/theme/app_colors.dart';
 import 'package:portfolio_website/core/helpers/responsive_helper.dart';
 import 'package:portfolio_website/presentation/viewModel/home_view_model.dart';
 
@@ -13,9 +14,17 @@ class PersonalImageWidget extends StatelessWidget {
     final isDeskTop = context.isDeskTop;
     return ConstrainedBox(
       constraints: BoxConstraints(maxHeight: isDeskTop ? 520 : 300),
-      child: CachedNetworkImage(
-        imageUrl: profileData!.personalInfo.photo,
-        placeholder: (context, url) => CircularProgressIndicator(),
+      child: Container(
+        color: AppColorsDark.black,
+        child: ClipOval(
+
+          child: CachedNetworkImage(
+            fit: BoxFit.cover,
+
+            imageUrl: profileData!.personalInfo.photo,
+            placeholder: (context, url) => CircularProgressIndicator(),
+          ),
+        ),
       ),
     );
   }
